2d79a860fd63ada38a6deb6c56b2982afb2be9e6
Muhammad Haseeb <haseebkhann7676@gmail.com>
2025-09-18 15:40:22 +0530

    Initial comment

---
+++ app.js
@@ -0,0 +1,21 @@
+const max = prompt("Enter the max number");
+const random = Math.floor(Math.random() * max) + 1;
+//console.log(random);
+
+let guess = prompt("Guess the number or type 'quit' to exit");
+
+while(true){
+     if(guess == 'quit'){
+        console.log('Quit The Game');
+        break;
+     }
+      if (guess == random){
+        console.log("Congratulation!! You Guessed Correct number");
+        break;
+     }else if(guess< random){
+       guess = prompt("hint: You guess is too small!! Please try again");
+     }else {
+       guess = prompt("hint: You guess is too large!! Please try again");
+     }
+     
+}
